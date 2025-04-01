f793759033cd83b29c87bc4205dc33fb9e67d21a
Rahul Kurmalkar <therahulkurmalkar@gmail.com>
2025-04-02 00:52:06 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -5,14 +5,16 @@ import docModel from "./document.model.js";
 dotenv.config();
 
 
-mongoose.connect(process.env.DB_URL).then(() => {    
-    console.log('Connected to MongoDB1');
-})
-.catch((error) => {
-    console.log('Error connecting to MongoDB:', error);
-});
-
-const io = new Server(process.env.PORT, {
+async function connectDB() {
+    await mongoose.connect(process.env.DB_URL).then(() => {
+        console.log('Connected to MongoDB');
+    })
+        .catch((error) => {
+            console.error('Error connecting to MongoDB:', error);
+        });
+}
+await connectDB();
+const io = new Server(3000, {
     cors: {
         origin: process.env.FRONTEND_URL,
         methods: ["GET", "POST"]
@@ -20,31 +22,27 @@ const io = new Server(process.env.PORT, {
 })
 
 io.on("connection", socket => {
-    socket.on('get-document', async (documentId)=>{
+    socket.on('get-document', async (documentId) => {
+        console.log("id: ", documentId);
         const document = await findOrCreateDoc(documentId);
         socket.join(documentId);
         socket.emit('load-document', document.doc);
-        socket.on("send-changes", (delta)=>{
+        socket.on("send-changes", (delta) => {
             socket.broadcast.to(documentId).emit("receive-changes", delta);
         })
-        socket.on("save-changes", async (doc)=>{
-            await saveAndUpdate(documentId, doc);
+        socket.on("save-changes", async (doc) => {
+            await docModel.findByIdAndUpdate(documentId, { doc });
         })
     })
 })
 
 async function findOrCreateDoc(docid) {
-    if(!docid) return ;
-    const findDoc = await docModel.findById(docid);
-    if(findDoc) return findDoc;
+    if (!docid) return;
+    const findDoc = await docModel.findById(docid).lean().exec();
+    if (findDoc) return findDoc;
     const createdDoc = await docModel.create({
         _id: docid,
         doc: ""
     });
     return createdDoc;
-}
-
-async function saveAndUpdate(docid, doc) {
-    if(!docid || !doc) return;
-    return await docModel.findByIdAndUpdate(docid, {doc});
 }
